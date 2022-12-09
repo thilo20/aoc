abea1280aa1a70621f67792cc86d216558cba3da
Thilo Schaper <thilo.schaper@cariad.technology>
2022-12-09 11:13:16 +0100

    solved day3

--- advent3/part2.go
+++ advent3/part2.go
@@ -4,112 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 	"unicode"
 
 	"k8s.io/apimachinery/pkg/util/sets"
 )
 
-type Shape int64
-
-const (
-	// since iota starts with 0, the first value
-	// defined here will be the default
-	Undefined Shape = iota
-	Rock
-	Paper
-	Scissors
-)
-
-func (s Shape) String() string {
-	switch s {
-	case Rock:
-		return "rock"
-	case Paper:
-		return "paper"
-	case Scissors:
-		return "scissors"
-	}
-	return "unknown"
-}
-
-func (s Shape) IntVal() int {
-	switch s {
-	case Rock:
-		return 1
-	case Paper:
-		return 2
-	case Scissors:
-		return 3
-	}
-	return 0
-}
-
-type Round struct {
-	opponent, you Shape
-	win           string
-	points        int
-}
-
-func (s Round) String() string {
-	return fmt.Sprintf("%s %s %s %d", s.opponent, s.you, s.win, s.points)
-}
-
-func CalcShape(opp Shape, win string) Shape {
-	switch {
-	case opp == Rock && win == "no":
-		return Scissors
-	case opp == Rock && win == "yes":
-		return Paper
-	case opp == Paper && win == "no":
-		return Rock
-	case opp == Paper && win == "yes":
-		return Scissors
-	case opp == Scissors && win == "no":
-		return Paper
-	case opp == Scissors && win == "yes":
-		return Rock
-	}
-	return opp
-}
-
-func CalcPoints(s Round) int {
-	points := 0
-	points += s.you.IntVal()
-	switch {
-	case s.win == "draw":
-		points += 3
-	case s.win == "yes":
-		points += 6
-	}
-	return points
-}
-
-func roundFromLine(line string) Round {
-	var r Round
-	switch {
-	case strings.HasPrefix(line, "A"):
-		r.opponent = Rock
-	case strings.HasPrefix(line, "B"):
-		r.opponent = Paper
-	case strings.HasPrefix(line, "C"):
-		r.opponent = Scissors
-	}
-	switch {
-	case strings.HasSuffix(line, "X"):
-		r.win = "no"
-	case strings.HasSuffix(line, "Y"):
-		r.win = "draw"
-	case strings.HasSuffix(line, "Z"):
-		r.win = "yes"
-	}
-
-	r.you = CalcShape(r.opponent, r.win)
-	r.points = CalcPoints(r)
-	return r
-}
-
-// day 3 part 1
+// day 3 part 2
 func main() {
 
 	filePath := os.Args[1]
@@ -121,19 +21,22 @@ func main() {
 	scanner := bufio.NewScanner(readFile)
 	scanner.Split(bufio.ScanLines)
 
+	elves := make([]string, 0)
+	lc := 0
 	max := 0
-	fmt.Println(Rock)
+
 	for scanner.Scan() {
 		line := scanner.Text()
+		lc++
 
-		left := line[:len(line)/2]
-		right := line[len(line)/2:]
-		fmt.Println(left, right)
+		elves = append(elves, line)
 
-		a3 := getDuplicate(left, right)
-
-		prio := getPriority(a3)
-		max += prio
+		if lc%3 == 0 {
+			a := getDuplicate(elves)
+			prio := getPriority(a)
+			max += prio
+			elves = make([]string, 0)
+		}
 	}
 	readFile.Close()
 
@@ -150,16 +53,22 @@ func getPriority(a string) int {
 	return 0
 }
 
-func getDuplicate(left string, right string) string {
+func getDuplicate(elves []string) string {
 	var a sets.String = sets.NewString()
-	for _, val := range left {
+	for _, val := range elves[0] {
 		a.Insert(string(val))
 	}
 	var a2 sets.String = sets.NewString()
-	for _, val := range right {
+	for _, val := range elves[1] {
 		a2.Insert(string(val))
 	}
-	a3 := a.Intersection(a2)
-	fmt.Println(a3.Len(), a3.List())
-	return a3.List()[0]
+	var a3 sets.String = sets.NewString()
+	for _, val := range elves[2] {
+		a3.Insert(string(val))
+	}
+
+	dupl := a.Intersection(a2)
+	dupl = dupl.Intersection(a3)
+	fmt.Println(dupl.Len(), dupl.List())
+	return dupl.List()[0]
 }
