b7f3b79e62754a29afb70f3b6f05a8a03478a8e4
Thilo Schaper <thilo.schaper@cariad.technology>
2022-12-12 23:46:47 +0100

    Revert "pretty output"
    This reverts commit e30a00cb21b17520d156db613d7051ab8b1a35dc.

--- day12/part1.go
+++ day12/part1.go
@@ -8,7 +8,7 @@ import (
 	"github.com/beefsack/go-astar"
 )
 
-// day 12 part 2
+// day 12 part 1
 func main() {
 
 	filePath := os.Args[1]
@@ -31,30 +31,17 @@ func main() {
 	fmt.Println("Parsed map:")
 	fmt.Println(world.RenderPath([]astar.Pather{}))
 
-	starts := world.Elevations('a')
-	fmt.Println("possible starting tiles: ", len(starts))
+	// t1 and t2 are *Tile objects from inside the world.
+	path, distance, found := astar.Path(world.From(), world.To())
+	if !found {
+		fmt.Println("Could not find path")
+	} else {
+		fmt.Println("Path found:")
+		fmt.Println(world.RenderPath(path))
 
-	var shortest_path []astar.Pather
-	var shortest_distance int = 2147483647
-
-	for k, from := range starts {
-		// t1 and t2 are *Tile objects from inside the world.
-		path, distance, found := astar.Path(from, world.To())
-		if !found {
-			fmt.Println("Could not find path", k)
-		} else {
-			fmt.Println(k, "Found path with distance ", distance)
-			if distance < float64(shortest_distance) {
-				shortest_distance = int(distance)
-				shortest_path = path
-			}
-		}
+		PrintPath(path)
+		fmt.Println("dist:", distance)
 	}
-	fmt.Println("Path found:")
-	fmt.Println(world.RenderPath(shortest_path))
-
-	PrintPath(shortest_path)
-	fmt.Println("dist:", shortest_distance)
 
 }
 
