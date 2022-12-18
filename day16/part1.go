fcba47d8872926434edcf9a3c6fafc928082d67b
Thilo Schaper <thilo.schaper@cariad.technology>
2022-12-18 22:50:40 +0100

    keep minute when valves opened; add logs

--- day16/part1.go
+++ day16/part1.go
@@ -8,10 +8,20 @@ import (
 	"strconv"
 )
 
-type Valve struct {
-	rate    int
-	tunnels []Valve
+// search state
+type State struct {
+	minute      int            //search step
+	flowrate    int            //aggregated open valves
+	valve       string         //where am I?
+	valve_prev  string         //where did I come from?
+	valves_open map[string]int // valves already opened, value: opened in minute x
+	pressure    int            //total pressure released
+	action      string         // open or move
 }
+
+// parsed network of valves and tunnels
+var tunnels = map[string]([]string){}
+var rates = map[string]int{}
 
 // day 16 part 1
 func main() {
@@ -21,10 +31,6 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	// parsed network of valves and tunnels
-	tunnels := map[string]([]string){}
-	rates := map[string]int{}
 
 	scanner := bufio.NewScanner(readFile)
 
@@ -40,7 +46,9 @@ func main() {
 		tunnels[m[0]] = m[1:]
 		rate := re2.FindString(line)
 		val, _ := strconv.Atoi(rate)
-		rates[m[0]] = val
+		if val > 0 { //add only valves where opening will increase flow rate
+			rates[m[0]] = val
+		}
 
 	}
 	readFile.Close()
@@ -48,4 +56,64 @@ func main() {
 	fmt.Println("tunnels:", tunnels)
 	fmt.Println("flow rates:", rates)
 
+	// breadth first search
+	nodes := []State{{0, 0, "AA", "AA", map[string]int{}, 0, ""}}
+	next := []State{}
+	minutes := 25
+	for minute := 0; minute < minutes; minute++ {
+		for _, node := range nodes {
+			next = append(next, expand_search(&node)...)
+		}
+		fmt.Println("minute:", minute, "nodes:", len(nodes), "next:", len(next))
+		nodes = next
+
+		calcMaxPressure(nodes, minute)
+	}
+
+}
+
+func expand_search(s *State) []State {
+	res := []State{}
+
+	opened := s.valves_open
+	flowrate := s.flowrate
+	if s.action == "open" {
+		opened = make(map[string]int)
+		for k, v := range s.valves_open {
+			opened[k] = v
+		}
+		opened[s.valve] = s.minute + 1
+		flowrate += rates[s.valve]
+		// fmt.Println("opened:", opened, "flowrate:", flowrate)
+		if len(opened) == len(rates) {
+			fmt.Println("all valves opened! min:", s.minute+1, "max flow:", flowrate)
+		}
+	}
+
+	if opened[s.valve] == 0 && rates[s.valve] > 0 {
+		res = append(res, State{s.minute + 1, flowrate, s.valve, s.valve, opened, s.pressure + flowrate, "open"})
+	}
+	for _, v := range tunnels[s.valve] {
+		if v == s.valve_prev {
+			continue //prevent cycles
+		}
+		res = append(res, State{s.minute + 1, flowrate, v, s.valve, opened, s.pressure + flowrate, "move"})
+	}
+	// fmt.Println("expanded:", len(res))
+	return res
+}
+
+func calcMaxPressure(nodes []State, minute int) {
+	max_pressure := 0
+	var s State
+	for _, v := range nodes {
+		// if v.pressure > 0 {
+		// 	fmt.Println("pres:", v.pressure)
+		// }
+		if v.pressure > max_pressure {
+			max_pressure = v.pressure
+			s = v
+		}
+	}
+	fmt.Printf("Max pressure after %d minutes: %d s=%v\n", minute, max_pressure, s)
 }
