f85e2bae4644185bb9a4b591e2f1451e9991fbbd
Thilo Schaper <thilo.schaper@cariad.technology>
2023-01-02 20:29:33 +0100

    init day25 parse, decode, sum

---
+++ day25/part1.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+// day 25 part 1
+func main() {
+
+	filePath := os.Args[1]
+	readFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	snafu := []string{}
+	sum := 0
+
+	scanner := bufio.NewScanner(readFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		snafu = append(snafu, line)
+
+		decimal := Decode(line)
+		snafu2 := Encode(decimal)
+		fmt.Printf("%10s\t%10d\t%10s\n", line, decimal, snafu2)
+		sum += decimal
+	}
+	readFile.Close()
+
+	snafusum := Encode(sum)
+	fmt.Println("sum:", sum, "snafu:", snafusum)
+}
+
+// decode snafu to decimal value
+func Decode(snafu string) int {
+	val := 0
+	for i := 0; i < len(snafu); i++ {
+		mult := int(math.Pow(5, float64(i)))
+		char := snafu[len(snafu)-1-i]
+		switch char {
+		case '2':
+			val += 2 * mult
+		case '1':
+			val += 1 * mult
+		case '0':
+			val += 0 * mult
+		case '-':
+			val -= 1 * mult
+		case '=':
+			val -= 2 * mult
+		}
+	}
+	return val
+}
+
+// decode snafu to decimal value
+func Encode(decimal int) string {
+	return "210-="
+}
