859c690a7ac26f076d9c65065b5d8674c1b3cd2b
Thilo Schaper <thilo.schaper@cariad.technology>
2023-12-04 00:14:43 +0100

    format

--- day2.py
+++ day2.py
@@ -7,25 +7,27 @@ sum = 0
 # Game (\d+): (((?'count'\d+) (?'color'\w+)[, ;]*)*)?;
 # (Game (?P<game>\d+): )?((?P<count>\d) (?P<color>\w+))(?P<token>[,;]?)
 
-bag={'red':12, 'green':13, 'blue':14}
+bag = {'red': 12, 'green': 13, 'blue': 14}
+
 
 def is_not_enough(color, count):
-    return bag[color]<count
+    return bag[color] < count
+
 
 for line in file:
-    #print("Line{}: {}".format(count, line.strip()))
-    add=int(re.findall("Game (?P<game>\d+): ", line)[0])
+    # print("Line{}: {}".format(count, line.strip()))
+    add = int(re.findall("Game (?P<game>\d+): ", line)[0])
 
-    r1=re.findall("((?P<count>\d+) (?P<color>\w+))(?P<token>[,;]?)", line)
+    r1 = re.findall("((?P<count>\d+) (?P<color>\w+))(?P<token>[,;]?)", line)
 
     for draw in r1:
         if is_not_enough(draw[2], int(draw[1])):
-            add=0
+            add = 0
             break
 
-    sum+=add
+    sum += add
 
-    #print(r1)
+    # print(r1)
 
 print(sum)
 file.close()
