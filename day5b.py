9ee210648e79f8bc61b8a1d1f7605a32246843e2
Thilo Schaper <thilo.schaper@cariad.technology>
2023-12-08 13:59:00 +0100

    day5 fail to solve forwards

--- day5b.py
+++ day5b.py
@@ -8,6 +8,8 @@ to_int = lambda a : int(a)
 seeds=[]
 mappings=[]
 
+x=list(range(0, 3))
+
 def resolve_destination(source, mapping):
     if mapping.get(source):
         return mapping[source][0]
@@ -20,7 +22,7 @@ def resolve_destination(source, mapping):
         if key>source:
             above=key
             break
-    print(below, above)
+    # print(below, above)
     
     if below >= 0:
         # check if source is within range
@@ -66,16 +68,44 @@ def resolve_location(seed, mappings):
     number=seed
     for item in mappings:
         destination=resolve_destination(number, item["map"])
-        msg="{} {}->{}"
-        print(msg.format(item["name"], number, destination))
+        # msg="{} {}->{}"
+        # print(msg.format(item["name"], number, destination))
         number=destination
     return number
+
+# apply seed range
+best=8375662680
+best_seed=0
+
+# 2:override seeds
+# seeds=[3151642679, 224376393]
+for i in range(1, len(seeds), 2):
+    if i!=5:
+        continue
+    # seeds2=list(range(seeds[i-1], seeds[i-1]+seeds[i]))
+    # seeds2=list(range(seeds[i-1], seeds[i-1]+seeds[i], 1000)) # 1:sample seeds
+    seeds2=list(range(seeds[i-1], seeds[i-1]+seeds[i], 100)) # 1:sample seeds
+
+    print("len seeds ranged: ", i, len(seeds2), seeds2[0])
+    # loc = min(map(lambda x: resolve_location(x, mappings), seeds2))
+    loc=best
+    s2=0
+    for seed in seeds2:
+        temp=resolve_location(seed, mappings)
+        if temp<loc:
+            loc=temp
+            s2=seed
+    print("loc:", loc, s2)
+    if loc < best:
+        best=loc
+        best_seed=s2
+        print("best i", i)
+
 
 # seed2location =dict()
 # for seed in seeds:
 #     t=resolve_location(seed, mappings)
 #     seed2location[seed]=t
 # loc=min(seed2location, key=seed2location.get)
-loc = min(map(lambda x: resolve_location(x, mappings), seeds))
 
-print(loc)
+print(best, best_seed)
