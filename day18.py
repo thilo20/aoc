f384dea31e879cf879e948c6636b9e3e82e85a47
Thilo Schaper <thilo.schaper@cariad.technology>
2023-12-20 15:17:37 +0100

    remove recursive version

--- day18.py
+++ day18.py
@@ -41,69 +41,7 @@ print(total)
 #      if image[0][i]>0: print(i)
 # 127,1
 
-# https://gist.github.com/shkolovy/a2c807e5ba2783ded3eedd55b9abf42c
-from queue import Queue
-def get_neightbs(array, pos):
-    neighbs = []
-
-    if pos[1] > 0:
-        neighbs.append((pos[0], pos[1] - 1))
-    if pos[0] > 0:
-        neighbs.append((pos[0] - 1, pos[1]))
-    if pos[1] < len(array[0]) - 1:
-        neighbs.append((pos[0], pos[1] + 1))
-    if pos[0] < len(array) - 1:
-        neighbs.append((pos[0] + 1, pos[1]))
-
-    return neighbs
-
-#pos (1,2) row, col
-def flood_fill(array, start_pos):
-    q = Queue()
-    q.put(start_pos)
-    val=array[start_pos[0]][start_pos[1]]
-
-    while not q.empty():
-        pos = q.get()
-        array[pos[0]][pos[1]] = val
-
-        neighbs = get_neightbs(array, pos)
-        for n in neighbs:
-            if array[n[0]][n[1]] == 0:
-                q.put(n)
-
-# flood_fill(image, (1,1))
-
-# from https://lvngd.com/blog/flood-fill-algorithm-python/
-def flood_recursive(matrix, start_pos):
-	width = len(matrix)
-	height = len(matrix[0])
-        
-	def fill(x,y,start_color,color_to_update):
-		#if the square is not the same color as the starting point
-		if matrix[x][y] != start_color:
-			return
-		#if the square is not the new color
-		elif matrix[x][y] == color_to_update:
-			return
-		else:
-			#update the color of the current square to the replacement color
-			matrix[x][y] = color_to_update
-			neighbors = [(x-1,y),(x+1,y),(x-1,y-1),(x+1,y+1),(x-1,y+1),(x+1,y-1),(x,y-1),(x,y+1)]
-			for n in neighbors:
-				if 0 <= n[0] <= width-1 and 0 <= n[1] <= height-1:
-					fill(n[0],n[1],start_color,color_to_update)
-	start_x = start_pos[0]
-	start_y = start_pos[1]
-	start_color = matrix[start_x][start_y]
-	fill(start_x,start_y,start_color,1)
-	return matrix
-
-# flood_recursive(image, (1,1))
-# fails with RecursionError: maximum recursion depth exceeded
-
 # https://yuminlee2.medium.com/flood-seed-fill-algorithm-21fba08a46e#9537
-
 from collections import deque
 class Solution(object):
     def floodFill(self, image, sr, sc, newColor):
