6391a2ed197968dd3093428b2bb65be9cb6682b5
Thilo Schaper <thilo.schaper@cariad.technology>
2024-01-02 20:53:11 +0100

    solve day 6 boat race parabola

---
+++ day6.py
@@ -0,0 +1,20 @@
+import re
+
+file = open("day6.txt")
+times= list(map(lambda x:int(x), re.findall("\d+", file.readline())))
+records= list(map(lambda x:int(x), re.findall("\d+", file.readline())))
+file.close()
+
+total=1
+for race in range(len(times)):
+    wins =1
+    # determine first t1 to beat the record: via sampling
+    t_race=times[race]
+    for t1 in range(1,t_race):
+        s=t1*(t_race-t1)
+        if s > records[race]:
+            wins = t_race - 2*t1 + 1
+            break
+    total*=wins
+
+print(total)
