a246e23399c0e3d960b7dfae9d58af6f64ae3e75
Thilo Schaper <thilo.schaper@cariad.technology>
2024-01-02 15:03:03 +0100

    rework loc->seed reverse mapping

--- day5b.py
+++ day5b.py
@@ -1,16 +1,14 @@
 import re
 
 # Opening file
-file = open('day5.txt', 'r')
+file = open('day5-test.txt', 'r')
 total = 0
 
 to_int = lambda a : int(a)
 seeds=[]
 mappings=[]
 
-x=list(range(0, 3))
-
-def resolve_destination(source, mapping):
+def resolve_source(source, mapping):
     if mapping.get(source):
         return mapping[source][0]
     # find keys below+above source
@@ -26,11 +24,32 @@ def resolve_destination(source, mapping):
     
     if below >= 0:
         # check if source is within range
-        if source <= below + mapping[below][1]:
+        if source < below + mapping[below][1]:
             # return destination with offset
             return mapping[below][0] + source-below
     # default noop
     return source
+
+def in_range(source, mapping):
+    if mapping.get(source):
+        return mapping[source][0]
+    # find keys below+above source
+    below=-1
+    above=-1
+    for key in mapping.keys():
+        if key<source:
+            below=key
+        if key>source:
+            above=key
+            break
+    # print(below, above)
+    
+    if below >= 0:
+        # check if source is within range
+        if source < below + mapping[below]:
+            return True
+    return False
+
 
 # init map
 for line in file:
@@ -41,12 +60,12 @@ for line in file:
         conv={}
         mappings.append({'name': line, 'map': conv})
     else:
-        # add mapping: source -> destination, range
+        # add mapping: destination -> source, range
         # 50 98 2
-        # file has destination range start of 50, a source range start of 98, and a range length
+        # The first line has a destination range start of 50, a source range start of 98, and a range length of 2
         values=list(map(to_int, re.findall("\d+", line)))
         if len(values)==3:
-            conv[values[1]]=[values[0], values[2]]
+            conv[values[0]]=[values[1], values[2]]
 file.close()
 
 print("initialized mappings:", len(mappings))
@@ -55,57 +74,45 @@ print("initialized mappings:", len(mappings))
 for item in mappings:
     item["map"]=dict(sorted(item["map"].items()))
 
-destination=resolve_destination(0, mappings[0]["map"])
-print(destination)
-destination=resolve_destination(50, mappings[0]["map"])
-print(destination)
-destination=resolve_destination(79, mappings[0]["map"])
-print(destination)
-destination=resolve_destination(98, mappings[0]["map"])
-print(destination)
+# init map for ranges of seeds
+seedranges={}
+for i in range(0,len(seeds)//2,1):
+    seedranges[seeds[2*i]]=seeds[2*i+1]
+seedranges=dict(sorted(seedranges.items()))
 
-def resolve_location(seed, mappings):
-    number=seed
-    for item in mappings:
-        destination=resolve_destination(number, item["map"])
-        # msg="{} {}->{}"
-        # print(msg.format(item["name"], number, destination))
-        number=destination
+source=resolve_source(0, mappings[0]["map"])
+print(source)
+source=resolve_source(50, mappings[0]["map"])
+print(source)
+source=resolve_source(79, mappings[0]["map"])
+print(source)
+source=resolve_source(98, mappings[0]["map"])
+print(source)
+source=resolve_source(100, mappings[0]["map"])
+print(source)
+
+msg="Seed number {} corresponds to soil number {}."
+print(msg.format(resolve_source(81, mappings[0]["map"]), 81))
+print(msg.format(resolve_source(14, mappings[0]["map"]), 14))
+print(msg.format(resolve_source(57, mappings[0]["map"]), 57))
+print(msg.format(resolve_source(13, mappings[0]["map"]), 13))
+
+
+def resolve_seed(location, mappings):
+    number=location
+    for item in reversed(mappings):
+        source=resolve_source(number, item["map"])
+        msg="{} {}->{}"
+        print(msg.format(item["name"], number, source))
+        number=source
     return number
 
-# apply seed range
-best=8375662680
-best_seed=0
+resolve_seed(35, mappings)
 
-# 2:override seeds
-# seeds=[3151642679, 224376393]
-for i in range(1, len(seeds), 2):
-    if i!=5:
-        continue
-    # seeds2=list(range(seeds[i-1], seeds[i-1]+seeds[i]))
-    # seeds2=list(range(seeds[i-1], seeds[i-1]+seeds[i], 1000)) # 1:sample seeds
-    seeds2=list(range(seeds[i-1], seeds[i-1]+seeds[i], 100)) # 1:sample seeds
+loc=0
+while True:
+    seed=resolve_seed(loc, mappings)
+    if in_range(seed, seedranges): break
+    loc+=1
 
-    print("len seeds ranged: ", i, len(seeds2), seeds2[0])
-    # loc = min(map(lambda x: resolve_location(x, mappings), seeds2))
-    loc=best
-    s2=0
-    for seed in seeds2:
-        temp=resolve_location(seed, mappings)
-        if temp<loc:
-            loc=temp
-            s2=seed
-    print("loc:", loc, s2)
-    if loc < best:
-        best=loc
-        best_seed=s2
-        print("best i", i)
-
-
-# seed2location =dict()
-# for seed in seeds:
-#     t=resolve_location(seed, mappings)
-#     seed2location[seed]=t
-# loc=min(seed2location, key=seed2location.get)
-
-print(best, best_seed)
+print(loc)
