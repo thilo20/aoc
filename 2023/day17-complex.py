ab4bf4ebe6f88aaefbea250741d84ef2b4ade65a
Thilo Schaper <thilo.schaper@cariad.technology>
2024-02-04 00:49:33 +0100

    comments for understanding

--- 2023/day17-complex.py
+++ 2023/day17-complex.py
@@ -1,23 +1,24 @@
 from heapq import heappop, heappush
 
+# i=y, k=x
 G = {i + k*1j: int(c) for i,r in enumerate(open('2023/day17-test.txt'))
                       for k,c in enumerate(r.strip())}
 
 def f(min, max, end=[*G][-1], x=0):
-    todo = [(0,0,0,1), (0,0,0,1j)]
+    todo = [(0,0,0,1)]#, (0,0,0,1j)]
     seen = set()
 
     while todo:
-        val, _, pos, dir = heappop(todo)
+        val, _, pos, dir = heappop(todo) # val=cost
 
         if (pos==end): return val
         if (pos, dir) in seen: continue
         seen.add((pos,dir))
 
-        for d in 1j/dir, -1j/dir:
+        for d in 1j/dir, -1j/dir: # 1.Drehung nach rechts/links an pos 0,0 bzw. 0+0j; d=j entspricht 'nach rechts' (x-Richtung)
             for i in range(min, max+1):
-                if pos+d*i in G:
-                    v = sum(G[pos+d*n] for n in range(1,i+1))
-                    heappush(todo, (val+v, (x:=x+1), pos+d*i, d))
+                if pos+d*i in G: # kompaktes ongrid
+                    v = sum(G[pos+d*n] for n in range(1,i+1)) # Kosten fuer Abschnitt Geradeausfahrt
+                    heappush(todo, (val+v, (x:=x+1), pos+d*i, d)) # x:= ist ein inline counter
 
 print(f(1, 3), f(4, 10))
